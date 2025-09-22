f956465b81623c66beb60fc4f6fb1d5c1b3edee6
SIZNING_GITHUB_USERNAME <SIZNING_EMAIL>
2025-09-23 00:01:07 +0500

    smarteducation db

--- config/settings.py
+++ config/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'db',             # 127.0.0.1
+        'HOST': 'localhost',             # 127.0.0.1
         'PORT': '5432',
     }
 }
